d688ab64ad31ca7a1a52348c49724b95f6d29b1a
Prudhvi Raj <76194727+prudhviraj-1729@users.noreply.github.com>
2022-12-23 18:25:33 +0530

    Time: 1 ms (53.29%), Space: 42.9 MB (39.44%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -4,10 +4,12 @@ class Solution {
         int b = n - 1;
         for(int i = nums1.length - 1; b >= 0; i--){
             if(a >= 0 && nums1[a] > nums2[b]){
-                nums1[i] = nums1[a--];
+                nums1[i] = nums1[a];
+                a--;
             }
             else{
-                nums1[i] = nums2[b--];
+                nums1[i] = nums2[b];
+                b--;
             }
         }
     }
