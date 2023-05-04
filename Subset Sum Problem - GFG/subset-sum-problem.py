13fb1af62fd2f0217b7056ab2204b08982c5638b
Prudhvi Raj <76194727+prudhviraj-1729@users.noreply.github.com>
2023-05-04 10:42:14 +0530

    Added solution - LeetHub

---
+++ Subset Sum Problem - GFG/subset-sum-problem.py
@@ -0,0 +1,48 @@
+#User function Template for python3
+
+class Solution:
+    def isSubsetSum (self, N, arr, sum):
+        # code here 
+        dp = [[0 for j in range(sum + 1)] for i in range(N + 1)]
+        for i in range(N + 1):
+            for j in range(sum + 1):
+                if i == 0:
+                    dp[i][j] = False
+                if j == 0:
+                    dp[i][j] = True
+    
+        for i in range(1, N + 1):
+            for j in range(1, sum + 1):
+                if arr[i - 1] <= j:
+                    dp[i][j] = dp[i - 1][j - arr[i - 1]] or dp[i - 1][j]
+                else:
+                    dp[i][j] = dp[i - 1][j]
+                
+        if dp[N][sum] == True:
+            return 1
+        return 0
+        
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        N = int(input())
+        arr = input().split()
+        for itr in range(N):
+            arr[itr] = int(arr[itr])
+        sum = int(input())
+
+        ob = Solution()
+        if ob.isSubsetSum(N,arr,sum)==True:
+            print(1)
+        else :
+            print(0)
+            
+        
+
+# } Driver Code Ends
