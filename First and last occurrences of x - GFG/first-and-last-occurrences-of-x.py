8cccca47bae409745cb5830d90eb2db0a7167aba
Prudhvi Raj <76194727+prudhviraj-1729@users.noreply.github.com>
2023-09-22 16:08:43 +0530

    Added solution - LeetHub

---
+++ First and last occurrences of x - GFG/first-and-last-occurrences-of-x.py
@@ -0,0 +1,49 @@
+#User function Template for python3
+
+
+class Solution:
+    def find(self, arr, n, x):
+        
+        # code here
+        def findFirst(nums, target):
+            l, r = 0, len(nums)
+            while l < r:
+                mid = l + (r - l) // 2
+                if nums[mid] < target:
+                    l = mid + 1
+                else:
+                    r = mid 
+            if l < len(arr) and arr[l] == target:
+                return l
+            return -1
+        
+        def findLast(nums, target):
+            l, r = 0, len(nums)
+            while l < r:
+                mid = l + (r - l) // 2
+                if nums[mid] <= target:
+                    l = mid + 1
+                else:
+                    r = mid
+            if r > 0 and arr[r - 1] == target:
+                return r - 1
+            return -1
+        
+        res = [0] * 2
+        res[0] = findFirst(arr, x)
+        res[1] = findLast(arr, x)
+        return res
+
+
+#{ 
+ # Driver Code Starts
+t=int(input())
+for _ in range(0,t):
+    l=list(map(int,input().split()))
+    n=l[0]
+    x=l[1]
+    arr=list(map(int,input().split()))
+    ob = Solution()
+    ans=ob.find(arr,n,x)
+    print(*ans)
+# } Driver Code Ends
