2e48932895822fed9e2c8bee5d3d0a5ea8624cc2
Prudhvi Raj <76194727+prudhviraj-1729@users.noreply.github.com>
2023-11-04 00:18:21 +0530

    Added solution - LeetHub

--- Number of Provinces - GFG/number-of-provinces.py
+++ Number of Provinces - GFG/number-of-provinces.py
@@ -1,36 +1,30 @@
 #User function Template for python3
-from collections import deque
+from collections import defaultdict
 
 class Solution:
     def numProvinces(self, adj, V):
         # code here 
         visited_array = ["false" for i in range(V + 1)]
-        graph_list = [[] for i in range(V + 1)]
-        for i in range(V):
-            for j in range(V):
-                if adj[i][j] == 1 and i != j:
-                    if j + 1 not in graph_list[i + 1]:
-                        graph_list[i + 1].append(j + 1)
-                    if i + 1 not in graph_list[j + 1]:
-                        graph_list[j + 1].append(i + 1)
-                        
-        def bfs(start_point, graph_list, visited_array):
-            queue = deque()
-            queue.append(start_point)
-            visited_array[start_point] = "true"
-            while queue:
-                x = queue.popleft()
-                for i in graph_list[x]:
-                    if visited_array[i] == "false":
-                        visited_array[i] = "true"
-                        queue.append(i)
+        graph_list = defaultdict(list)
+        for i in range(len(adj)):
+            for j in range(i + 1, len(adj)):
+                if adj[i][j] == 1:
+                    graph_list[i + 1].append(j + 1)
+                    graph_list[j + 1].append(i + 1)
+        
+        def dfs(index, graph_list, visited_array):
+            visited_array[index] = "true"
+            for i in graph_list[index]:
+                if visited_array[i] == "false":
+                    dfs(i, graph_list, visited_array)
         
         count = 0
         for i in range(1, len(visited_array)):
             if visited_array[i] == "false":
-                bfs(i, graph_list, visited_array)
+                dfs(i, graph_list, visited_array)
                 count += 1
         return count
+
 #{ 
  # Driver Code Starts
 #Initial Template for Python 3
